611a6136df636a70d547bdf8e5e68fad434c7b01
1959908 <1959908@01hw1799558>
2021-11-11 19:19:32 +0530

    commit

---
+++ Demo/src/com/test/demo/Addition.java
@@ -0,0 +1,15 @@
+package com.test.demo;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=15;
+		b=20;
+		c=a+b;
+		System.out.println("The addition of two numbers is "+c);
+
+	}
+
+}
